33c85a535091e72c08f63992d8ff8324d7b62457
Mohamad <moeman.bdeir09@gmail.com>
2025-11-09 13:57:34 -0500

    Parsed response, organize

--- ollama_server/qwen_server.py
+++ ollama_server/qwen_server.py
@@ -1,14 +1,23 @@
 from ollama import chat, ChatResponse
 
-response: ChatResponse = chat(model='hf.co/Qwen/Qwen3-1.7B-GGUF:latest', messages=[
-  {
-    'role': 'user',
-    'content': 'What did i ask you previously?/nothink',
-    'format': 'json'
-  },
-],
-  stream=True
-)
+messages = [
+    {
+        'role': 'user',
+        'content': 'What did i ask you previously?/nothink',
+        'format': 'json'
+    },
+]
 
-for line in response:
-    print(line.message.content, end='', flush=True)
+def ask(message):
+    messages.append({'role': 'user', 'content': message, 'format': 'json'})
+    response: ChatResponse = chat(model='hf.co/Qwen/Qwen3-1.7B-GGUF:latest', messages=[*messages], stream=True)
+    # This stores ther response
+    qwen_response = ""
+    for line in response:
+        chunk = line.message.content.replace("<think>", '').replace("</think>", '').replace("\n", "")
+        qwen_response+= chunk
+        # This one prints it
+        print(chunk, end='', flush=True)
+    messages.append({'role': "assistant", 'content': response, qwen_response: 'json'})
+
+ask('How are you')
