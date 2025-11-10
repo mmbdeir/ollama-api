de5e3eb81a7f26032d1b8f426ffeb01cd15e2257
Mohamad <moeman.bdeir09@gmail.com>
2025-11-09 19:11:51 -0500

    Chat history, removed thinking tags, and streamed

--- ollama_server/qwen_server.py
+++ ollama_server/qwen_server.py
@@ -1,23 +1,19 @@
 from ollama import chat, ChatResponse
 
-messages = [
-    {
-        'role': 'user',
-        'content': 'What did i ask you previously?/nothink',
-        'format': 'json'
-    },
-]
+messages = []
 
 def ask(message):
-    messages.append({'role': 'user', 'content': message, 'format': 'json'})
+    messages.append({'role': 'user', 'content': f'{message}/no_think', 'format': 'json'})
     response: ChatResponse = chat(model='hf.co/Qwen/Qwen3-1.7B-GGUF:latest', messages=[*messages], stream=True)
     # This stores ther response
     qwen_response = ""
     for line in response:
-        chunk = line.message.content.replace("<think>", '').replace("</think>", '').replace("\n", "")
+        chunk = line.message.content.replace("<think>", '').replace("</think>", '').replace("\n", "").replace("/nothink", "")
         qwen_response+= chunk
         # This one prints it
         print(chunk, end='', flush=True)
-    messages.append({'role': "assistant", 'content': response, qwen_response: 'json'})
+    messages.append({'role': "assistant", 'content': qwen_response, 'format': 'json'})
+    print(messages)
 
-ask('How are you')
+ask('Very explain why the sky is blue')
+ask('Relate that to my eyes')
